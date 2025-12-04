b17b23df091ac4c75c581c5bbf91db6fe1cb1e20
alex_stavrin <alekosstavrinoudakis@gmail.com>
2025-12-04 08:43:27 +0200

    finished day 4

---
+++ day_04/star_2.lua
@@ -0,0 +1,128 @@
+local input_file = "input.txt"
+
+local function check_pos_for_paper(i, j, map)
+    
+    local number_of_rows = #map
+    local number_of_columns = # map[1]
+
+    if i >= 1 and i <= number_of_rows and j >= 1 and j <= number_of_columns then
+        if map[i][j] == '@' then
+            return true
+        end
+    end
+
+    return false
+end
+
+local function can_lift_paper_roll(i, j, map)
+
+    local paper_count = 0
+
+    -- check right
+    if check_pos_for_paper(i, j+1,map) then
+        paper_count = paper_count + 1
+    end
+
+    -- check left
+    if check_pos_for_paper(i, j-1, map) then
+        paper_count = paper_count + 1
+    end
+
+    -- check bottom
+    if check_pos_for_paper(i+1,j,map) then
+        paper_count = paper_count + 1
+    end
+
+    -- check top
+    if check_pos_for_paper(i-1, j, map) then
+        paper_count = paper_count + 1
+    end
+
+    -- check diagonal top right
+    if check_pos_for_paper(i-1, j+1, map) then
+        paper_count = paper_count + 1
+    end
+
+    -- check diagonal top left
+    if check_pos_for_paper(i-1, j-1, map) then
+        paper_count = paper_count + 1
+    end
+
+    -- check diagonal bottom right
+    if check_pos_for_paper(i+1, j+1, map) then
+        paper_count = paper_count + 1
+    end
+
+    -- check diagonal bottom left
+    if check_pos_for_paper(i+1, j-1, map) then 
+        paper_count = paper_count + 1
+    end
+
+    if paper_count < 4 then
+        return true
+    end
+
+    return false
+end
+
+local file = io.open(input_file, "r")
+local map = {}
+
+if file then
+
+    local row_index = 1
+
+    for line in file:lines() do
+        map[row_index] = {}
+        for i = 1, #line do
+            map[row_index][i] = string.sub(line,i,i)
+        end
+
+        row_index = row_index + 1
+    end
+
+    file:close()
+end
+
+
+local can_be_lifted_count = 0
+local coordinates_to_be_removed = {}
+while true do
+    for i, row in ipairs(map) do
+        local line = ""
+        for j, char in ipairs(row) do
+            line = line .. char
+            if char == '@' then
+                local res = can_lift_paper_roll(i, j, map)
+                if res then
+                    can_be_lifted_count = can_be_lifted_count + 1
+                    local coordinates = {i, j}
+                    table.insert(coordinates_to_be_removed, coordinates)
+                end
+            end
+        end
+        -- print(line)
+    end
+
+    if #coordinates_to_be_removed == 0 then
+        goto out
+    end
+
+    for i, coordinates in ipairs(coordinates_to_be_removed) do
+        map[coordinates[1]][coordinates[2]] = 'x'
+    end
+
+    coordinates_to_be_removed = {}
+
+    -- for i, row in ipairs(map) do
+    --     local line = ""
+    --     for j, char in ipairs(row) do
+    --         line = line .. char
+    --     end
+    --     print(line)
+    -- end
+end
+
+::out::
+
+print(can_be_lifted_count)
